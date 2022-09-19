9263a8dbf33cdf0bc2dd776272e1432ab88e2ccb
Diogo-Miranda <damiranda@sga.pucminas.br>
2022-09-19 20:08:04 -0300

    configure webapi project

---
+++ webservices/web-api-cpu-process/src/main/java/com/tiv/webapicpuprocess/WebApiCpuProcessApplication.java
@@ -0,0 +1,13 @@
+package com.tiv.webapicpuprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiCpuProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiCpuProcessApplication.class, args);
+	}
+
+}
---
+++ webservices/web-api-cpu-process/src/test/java/com/tiv/webapicpuprocess/WebApiCpuProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiv.webapicpuprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiCpuProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
