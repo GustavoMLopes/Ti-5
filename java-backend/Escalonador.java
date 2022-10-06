e26587ea77fe517126fcaa5dd5d17c3286c142a4
Gustavo Martins <gumartinslopes@gmail.com>
2022-10-06 16:57:06 -0300

    Update Metricas

--- java-backend/Escalonador.java
+++ java-backend/Escalonador.java
@@ -1,23 +1,35 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Escalonador {
-    public static double utilizacaoCpu;
-    public static double trhoughput;
-    public static int trocaContexto;
-    public static int tempoGlobal;
+    public static double utilizacaoCpu = 0;
+    public static double trhoughput = 0;
+    public static int trocaContexto = 0;
+    public static int tempoGlobal = 0;
+
+    public static String descricaoExecucao = "Nenhum algoritmo executado ainda";
+    public static String caminhoLog = "";
+    public static String caminhoDiretorioLog = "logs/";
+    public static BufferedWriter fr;
     public static ArrayList<Processo> listaProcessos;
 
     public static void main(String[] args){
-        utilizacaoCpu = 0;
-        trocaContexto = 0;
-        trhoughput = 0;
+        File file = new File(caminhoDiretorioLog);  
+        if (!file.exists()) {
+            file.mkdir();
+        }
         listaProcessos = new ArrayList<>();
-
-        Processo p1 = new Processo("1",40, 10, 10);
-        Processo p2 = new Processo("2",40, 0, 10);
-        Processo p3 = new Processo("3",10, 20, 10);
-        Processo p4 = new Processo("4",10, 0, 10);
+        String escolhaAlgoritmo = (args.length > 0) ? args[0]: "";
+        
+        Processo p1 = new Processo("1",10, 0, 10);
+        Processo p2 = new Processo("2",10, 2, 10);
+        Processo p3 = new Processo("3",40, 3, 5);
+        Processo p4 = new Processo("4",10, 4, 1);
+        
         
         listaProcessos.add(p1);
         listaProcessos.add(p2);
@@ -25,11 +37,39 @@ public class Escalonador {
         listaProcessos.add(p4);
         Collections.sort(listaProcessos);
 
-        //fifo();
+        try{
+            validaEscolha(escolhaAlgoritmo);
+            calculaMetricas();
+            geraLog();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-        sjf();
-        calculaMetricas();
-        mostraMetricas();
+    public static void validaEscolha(String escolha) throws IOException{
+        
+        if( escolha.equals("SJF") || escolha.equals("sjf")){
+            System.out.println("****** SHORTEST JOB FIRST ******");
+            descricaoExecucao = "";
+            caminhoLog = "logs/sjf.log";
+            sjf();
+
+        }
+
+        else if(escolha.equals("RR") || escolha.equals("ROUND-ROBIN") || escolha.equals("round-robin")){
+            System.out.println("****** ROUND ROBIN ******");
+            descricaoExecucao = "";
+            caminhoLog = "logs/round_robin.log";
+            //ainda a ser implementado
+            //roundRobin();
+        }
+
+        else{
+            System.out.println("****** FIRST COME FIRST SERVICE ******");
+            descricaoExecucao = "";
+            caminhoLog = "logs/fcfs.log";
+            fifo();
+        }
     }
 
     public static void mostraLista(ArrayList<Processo> lista){
@@ -40,73 +80,116 @@ public class Escalonador {
         System.out.println("]");
     }
 
-    public static void mostraMetricas(){
-        System.out.println(" \n\n*** Estatisticas do escalonador *** ");
-        System.out.println("Utilizacao cpu: " + utilizacaoCpu + " %");
-        System.out.println("Throughput: " + trhoughput);
-        System.out.println("Quantidade de processos: " + listaProcessos.size());
-        System.out.println("Tempo gasto para a execucao do algoritmo: " + tempoGlobal);
+    public static void geraLog() throws IOException{
+        fr = new BufferedWriter(new FileWriter(caminhoLog));
+        fr.write("*** Lista de Processos ***\n");
+        for(Processo p : listaProcessos){
+            fr.write("------------\n");
+            fr.write(p.toString());
+            
+        }
+
+        fr.write("\n*** Descrição da execução ***\n");
+        fr.write(descricaoExecucao);
+
+        fr.write(" \n*** Estatisticas do escalonador ***\n");
+        fr.write("Utilizacao da cpu: " + utilizacaoCpu + "%\n");
+        fr.write("Throughput: " + trhoughput + "\n");
+        fr.write("Quantidade de processos: " + listaProcessos.size() + "\n");
+        fr.write("Tempo gasto para a execucao do algoritmo: " + tempoGlobal + "\n");
+        fr.write("Delay de troca de contexto: " + trocaContexto + "\n");
+        fr.close();
     }
 
-    public static void fifo(){
+    public static void fifo() throws IOException {
+        fr = new BufferedWriter(new FileWriter(caminhoLog));
+        fr.write("***** FCFS *****\n");
         ArrayList<Processo> listaProntos = new ArrayList<Processo>();
         Processo atual = listaProcessos.get(0);
-        listaProntos.add(atual);
+        //listaProntos.add(atual);
         tempoGlobal = atual.getArrivalTime();
         int []temposChegada = montaListaTemposChegada();
-        int indiceChegada = 1;
-        System.out.println("Processo " + listaProntos.get(0).getPid() + " executando...");
-        while(listaProntos.size() > 0){
-            
+        int indiceChegada = 0;
+        do{
+            //chegada
             while(indiceChegada < temposChegada.length && tempoGlobal == temposChegada[indiceChegada]){
-                System.out.println("Processo " + listaProcessos.get(indiceChegada).getPid() + " chegou no tempo " + temposChegada[indiceChegada]);
+                descricaoExecucao += ("Processo " + listaProcessos.get(indiceChegada).getPid() + " chegou no tempo " + temposChegada[indiceChegada] + "\n");
                 listaProntos.add(listaProcessos.get(indiceChegada));
+                if(indiceChegada == 0){
+                    atual.executa();
+                    descricaoExecucao += ("-> Processo " + listaProntos.get(0).getPid() + " executando...\n");
+                }
                 indiceChegada++;
             }
 
+            //escalonamento
             if(atual.getExecTime() >= atual.getBurstTime()){
                 Processo removido = listaProntos.remove(0);
-                System.out.println("O processo " + removido.getPid() + " terminou de executar no tempo " + tempoGlobal + " e gastou " + atual.getExecTime());
+                removido.finalizaProcesso(tempoGlobal);
+                removido.setAwaitTime();
+                //listaProcessos.get(listaProcessos.indexOf(removido)).finalizaProcesso(tempoGlobal);
+                
+                descricaoExecucao += ("O processo " + removido.getPid() + " terminou de executar no tempo " + tempoGlobal + " e gastou " + atual.getExecTime() + " ut\n\n");
                 
                 tempoGlobal += trocaContexto; 
                 if(listaProntos.size() > 0){
                     atual = listaProntos.get(0);
-                    System.out.println("Processo " + listaProntos.get(0).getPid() + " executando...");
+                    atual.executa();
+                    descricaoExecucao += ("-> Processo " + listaProntos.get(0).getPid() + " executando...\n");
                 }
             }
+
             if(listaProntos.size() > 0){
                 tempoGlobal++;
                 atual.incrementExecTime();
             }
-        };
+            
+        }while(listaProntos.size() > 0);
+        fr.close();
     }
 
     public static void sjf(){
         ArrayList<Processo> listaProntos = new ArrayList<Processo>();
         Processo atual = listaProcessos.get(0);
-        listaProntos.add(atual);
+        //listaProntos.add(atual);
         tempoGlobal = atual.getArrivalTime();
         int []temposChegada = montaListaTemposChegada();
-        int indiceChegada = 1;
-        System.out.println("Processo " + listaProntos.get(0).getPid() + " executando...");
-        while(listaProntos.size() > 0){
-            System.out.println("Atual: " + atual.getPid());
+        int indiceChegada = 0;
+        do{
             while(indiceChegada < temposChegada.length && tempoGlobal == temposChegada[indiceChegada]){
-                System.out.println("Processo " + listaProcessos.get(indiceChegada).getPid() + " chegou no tempo " + temposChegada[indiceChegada]);
+                descricaoExecucao += ("Processo " + listaProcessos.get(indiceChegada).getPid() + " chegou no tempo " + temposChegada[indiceChegada] + "\n");
                 listaProntos.add(listaProcessos.get(indiceChegada));
                 indiceChegada++;
                 
-                atual = getMenorBurstTime(listaProntos);
-                
+                if(indiceChegada == 0){
+                    atual.executa();
+                    descricaoExecucao += ("-> Processo " + atual.getPid() + " executando...\n");
+                }
+                //escalonamento se chegar outro processo
+                else{
+                    Processo menor = getMenorBurstTime(listaProntos);
+                    if(menor != atual){
+                        atual.prontificaProcesso();
+                        listaProntos.add(atual);
+                        atual = menor;
+                        atual.executa();
+                        descricaoExecucao += ("-> Processo " + atual.getPid() + " executando...\n");
+                    }
+                }
             }
 
+            //escalonamento se o processo acabar
             if(atual.getExecTime() >= atual.getBurstTime()){
                 Processo removido = listaProntos.remove(listaProntos.indexOf(atual));
-                System.out.println("O processo " + removido.getPid() + " terminou de executar no tempo " + tempoGlobal + " e gastou " + atual.getExecTime());
+                removido.finalizaProcesso(tempoGlobal);
+                removido.setAwaitTime();
+                //listaProcessos.get(listaProcessos.indexOf(removido)).finalizaProcesso(tempoGlobal);
+                descricaoExecucao += ("O processo " + removido.getPid() + " terminou de executar no tempo " + tempoGlobal + " e gastou " + atual.getExecTime() + "\n");
                 tempoGlobal += trocaContexto; 
                 if(listaProntos.size() > 0){
                     atual = getMenorBurstTime(listaProntos);
-                    System.out.println("Processo " + listaProntos.get(0).getPid() + " executando...");
+                    atual.executa();
+                    descricaoExecucao += ("-> Processo " + atual.getPid() + " executando...\n");
                 }
             }
 
@@ -114,7 +197,7 @@ public class Escalonador {
                 tempoGlobal++;
                 atual.incrementExecTime();
             }
-        };
+        }while(listaProntos.size() > 0);
     }
 
     public static Processo getMenorBurstTime(ArrayList<Processo> listaProntos){
--- java-backend/Processo.java
+++ java-backend/Processo.java
@@ -2,24 +2,35 @@ public class Processo implements Comparable<Processo>{
     
     private String pid;
     private int burstTime, arrivalTime;
-    private int state;
+    private String state;
     private int quantum;
     private int execTime;
+    private int awaitTime;
+    private int finishTime;
 
     public Processo(String pid, int burstTime, int arrivalTime, int quantum) {
         this.pid = pid;
         this.burstTime = burstTime;
         this.arrivalTime = arrivalTime;
-        this.state = 0;
+        this.state = "Pronto";
         this.quantum = quantum;
         this.execTime = 0;
+        this.awaitTime = 0;
+        this.finishTime = 0;
     }
 
     public String toString(){
         String rep = "PID: " + this.pid +"\n";
+        rep += "Tempo de chegada: " + this.arrivalTime + "\n";
         rep += "Burst Time: " + this.burstTime + "\n";
-        rep += "Arrival Time: " + this.arrivalTime + "\n";
-        rep += "State: " + this.state + "\n";
+        if(this.state.equals("Finalizado")){
+            rep += "Tempo de saida: " + this.finishTime + "\n";
+            rep+= "Tempo de execucao: " + this.execTime + "\n";
+            rep+= "Tempo de espera: " + this.awaitTime + "\n";
+        }
+        
+        //rep += "Quantum: " + this.quantum + "\n"; 
+        //rep += "State: " + this.state + "\n";
         return rep;
     }
 
@@ -27,9 +38,11 @@ public class Processo implements Comparable<Processo>{
     public String getPid(){ return this.pid; } 
     public int getBurstTime(){ return this.burstTime; } 
     public int getArrivalTime(){ return this.arrivalTime; } 
-    public int getState(){ return this.state; } 
+    public String getState(){ return this.state; } 
     public int getQuantum(){ return this.quantum; } 
     public int getExecTime(){ return this.execTime; }
+    public int getAwaitTime(){ return this.awaitTime; }
+    public int getFinishTime(){ return this.finishTime; }
     
     /* setters */
     public void setPid(String pid){
@@ -44,7 +57,7 @@ public class Processo implements Comparable<Processo>{
         this.arrivalTime = arrivalTime;
     }
 
-    public void setState(int state){
+    private void setState(String state){
         this.state = state;
     }
 
@@ -55,6 +68,25 @@ public class Processo implements Comparable<Processo>{
     public void incrementExecTime(){
         this.execTime = this.execTime + 1;
     }
+
+    public void setAwaitTime(){
+        System.out.println(this.finishTime + " - " + this.arrivalTime + " - " + this.burstTime);
+        this.awaitTime = this.finishTime - this.arrivalTime - this.burstTime; 
+    }
+
+    private void setFinishTime(int finishTime){
+        this.finishTime = finishTime;
+    }
+
+    // atualizadores do estado do processo
+    public void finalizaProcesso(int tempoFinalizado){ 
+        this.setState("Finalizado");
+        this.setFinishTime(tempoFinalizado); 
+    }
+
+    public void bloqueiaProcesso(){ this.setState("Bloqueado"); }
+    public void prontificaProcesso(){ this.setState("Pronto"); }
+    public void executa(){ this.setState("Executando"); }
 
     @Override
     public int compareTo(Processo p) {
